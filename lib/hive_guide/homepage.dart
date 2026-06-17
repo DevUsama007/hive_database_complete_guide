d53722d6686c1e66548499f67932fef855cc05d8
DevUsama007 <uuusama0026@gmail.com>
2026-06-17 11:51:51 +0500

    code to create the box and read the data from the box is added

--- lib/hive_guide/homepage.dart
+++ lib/hive_guide/homepage.dart
@@ -24,9 +24,18 @@ class _HomepageState extends State<Homepage> {
         onPressed: () async {
           var box = await Hive.openBox('intro');
           box.put('name', "Dev Usama");
+          box.put('Coding_languages', {
+            'Language 1': 'Dart',
+            'Language 2': 'Python',
+            'Language 3': 'C++',
+            'Language 4': 'Java',
+            'Language 5': 'C',
+          });
           setState(() {
             name = box.get('name');
           });
+          print(box.get('Coding_languages'));
+          print(box.get('Coding_languages')['Language 1']);
         },
         backgroundColor: Colors.deepPurple,
         child: const Icon(Icons.add, color: Colors.white),
@@ -36,8 +45,33 @@ class _HomepageState extends State<Homepage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Name is ${name ?? "Not set"}'),
-            const Text('Welcome to Hive Database Guide!'),
+            FutureBuilder(
+              future: Hive.openBox('intro'),
+              builder: (context, snapshot) {
+                // 1. Check if the asynchronous connection is still waiting
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(color: Colors.deepPurple),
+                  );
+                }
+
+                // 2. Check if an error occurred while opening the box
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      child: Text(
+                        snapshot.data!.get('name').toString() ?? 'searching',
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         ),
       ),
