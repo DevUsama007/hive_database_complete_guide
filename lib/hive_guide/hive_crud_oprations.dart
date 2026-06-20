a45c7d5f705863d9d8f48cea81978f29ed2abb78
DevUsama007 <uuusama0026@gmail.com>
2026-06-20 18:03:56 +0500

    hive crud oprations initial code is updated here

---
+++ lib/hive_guide/hive_crud_oprations.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HiveCrudOpration extends StatefulWidget {
+  const HiveCrudOpration({super.key});
+
+  @override
+  State<HiveCrudOpration> createState() => _HiveCrudOprationState();
+}
+
+class _HiveCrudOprationState extends State<HiveCrudOpration> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_database_guide/hive_guide/homepage.dart';
+import 'package:hive_database_guide/hive_guide/models/notes_model.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   var path = await getApplicationDocumentsDirectory();
   Hive.init(path.path);
+  Hive.registerAdapter(NotesModelAdapter());
+  await Hive.openBox<NotesModel>('notes');
   runApp(const MyApp());
 }
 
