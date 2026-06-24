953a5c92f4f3a1b62e0722fab6682456224e1ccf
DevUsama007 <uuusama0026@gmail.com>
2026-06-24 18:06:50 +0500

    some new example code snipets added for practice

--- lib/hive_guide/boxes/boxes.dart
+++ lib/hive_guide/boxes/boxes.dart
@@ -1,6 +1,9 @@
 import 'package:hive/hive.dart';
+import 'package:hive_database_guide/hive_guide/models/customer_model.dart';
 import 'package:hive_database_guide/hive_guide/models/notes_model.dart';
 
 class Boxes {
   static Box<NotesModel> getNotes() => Hive.box<NotesModel>('notes');
+  static Box<CustomerModel> getCustomer() =>
+      Hive.box<CustomerModel>('customer_data)');
 }
---
+++ lib/hive_guide/models/customer_model.dart
@@ -0,0 +1,10 @@
+import 'package:hive/hive.dart';
+part 'customer_model.g.dart';
+@HiveType(typeId: 2)
+class CustomerModel{
+  @HiveField(0)
+  String name;
+  @HiveField(1)
+  String address;
+  CustomerModel({required this.name,required this.address});
+}
